ea6d6585a91e668fb4ae25b4b8bacb9d3d8b39b3
eliasmurcray <eliasmurcray@gmail.com>
2024-03-10 00:45:05 -0800

    Update app.js, style.css

--- app.js
+++ app.js
@@ -1,58 +1,28 @@
-let toggle = document.querySelector("#header .toggle-button");
-let collapse = document.querySelectorAll("#header .collapse");
-
-toggle.addEventListener('click' , function(){
-    collapse.forEach(col => col.classList.toggle("collapse-toggle"));
-})
-
-// with masonry
-new Masonry("#posts .grid", {
-    itemSelector : '.grid-item',
-    gutter : 20
-});
-
-// swiper libray initialization
-new Swiper('.swiper-container', {
-    direction : 'horizontal',
-    loop : true,
-    slidesPerView : 5,
-    autoplay : {
-        delay : 3000
-    },
-    // responsive brakpoints
-    breakpoints : {
-        '@0' : {
-            slidesPerView : 2
-        },
-        // 888px
-        '@1.00' : {
-            slidesPerView : 3
-        },
-        // 1110px
-        '@1.25' : {
-            slidesPerView : 4
-        },
-        // 1330px
-        '@1.50' : {
-            slidesPerView: 5
-        }
-    }
-})
-
-// Sticky Navigation
-window.onscroll = function(){ myFunction()};
-
-// get the current value 
-let navbar = document.getElementById("header");
-
-// get the navbar position
-let sticky = navbar.offsetTop;
-
-// sticky function
-function myFunction(){
-    if(window.pageYOffset >= sticky){
-        navbar.classList.add("sticky");
-    }else{
-        navbar.classList.remove("sticky");
-    }
+let toggle = document.querySelector("#header .toggle-button"),
+	collapse = document.querySelectorAll("#header .collapse");
+toggle.addEventListener("click", function () {
+	collapse.forEach((e) => e.classList.toggle("collapse-toggle"));
+}),
+	new Masonry("#posts .grid", { itemSelector: ".grid-item", gutter: 20 }),
+	new Swiper(".swiper-container", {
+		direction: "horizontal",
+		loop: !0,
+		slidesPerView: 5,
+		autoplay: { delay: 3e3 },
+		breakpoints: {
+			"@0": { slidesPerView: 2 },
+			"@1.00": { slidesPerView: 3 },
+			"@1.25": { slidesPerView: 4 },
+			"@1.50": { slidesPerView: 5 },
+		},
+	}),
+	(window.onscroll = function () {
+		myFunction();
+	});
+let navbar = document.getElementById("header"),
+	sticky = navbar.offsetTop;
+function myFunction() {
+	window.pageYOffset >= sticky
+		? navbar.classList.add("sticky")
+		: navbar.classList.remove("sticky");
 }
